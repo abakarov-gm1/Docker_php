cd1dd8eaf3954a71a481cfc0eee03a8442262851
gadji <marat.tagirov.18@bk.ru>
2023-09-05 10:22:30 +0300

    first commit

---
+++ src/index.php
@@ -0,0 +1,78 @@
+<?php
+//    $hostname = 'localhost';
+//    $username = 'root';
+//    $password = '1234';
+//    $database = 'abakarov_test';
+//
+//    $link = mysqli_connect($hostname, $username, $password, $database);
+//    mysqli_query($link, "SET NAMES 'utf8'");
+//
+//    $query = 'SElECT name, age FROM user_test';
+//
+//    $res = mysqli_query($link, $query);
+//
+//$data = [];
+//
+//$t = mysqli_fetch_assoc($res);
+//$data[] = $t;
+//
+//$t = mysqli_fetch_assoc($res);
+//$data[] = $t;
+//
+//$t = mysqli_fetch_assoc($res);
+//$data[] = $t;
+//
+//
+//$t = mysqli_fetch_assoc($res);
+//$data[] = $t;
+//
+//
+//
+//
+////var_dump($data);
+//
+//foreach ($data as $elem) {
+////    echo $elem["name"] . ' ' . $elem["age"] ."\n";
+//    foreach ($elem as $item) {
+//        echo $item .PHP_EOL;
+//    }
+//}
+
+
+
+
+//$hostname = 'localhost';
+//$username = 'root';
+//$password = '1234';
+//$database = 'abakarov_test';
+
+//$link = mysqli_connect($hostname, $username, $password, $database);
+//$query = 'SElECT name, age FROM user_test';
+//$add = "INSERT INTO user_test (name, age, birthday) VALUES ('Джексон','13','2010-01-01')";
+//$update = "UPDATE user_test SET age=40 WHERE age = 13";
+//$delete = "DELETE FROM user_test WHERE name='Джексон'";
+//$q = "SELECT * FROM user_test LIMIT 1,2";
+//$res = mysqli_query($link, $q);
+
+//
+//$row = mysqli_fetch_assoc($res);
+//var_dump($row);
+//
+//$row = mysqli_fetch_assoc($res);
+//var_dump($row);
+//
+//$row = mysqli_fetch_assoc($res);
+//var_dump($row);
+
+
+
+
+
+
+$connection = mysqli_connect('mysql', 'root', 'root');
+
+
+
+
+
+
