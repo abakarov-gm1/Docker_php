1a92fc9a99c94d927bbd563919483ff6217f0204
Gadjimurad <you@example.com>
2023-09-08 17:24:16 +0300

    fix database

--- src/index.php
+++ src/index.php
@@ -10,14 +10,6 @@
             $connection->query($sql);
             echo "Вы авторизованны";
 
-
-//            $sql_fech = "SELECT * FROM users WHERE LastName=$_POST[name] AND FirstName=$_POST[password]";
-//            $result = $connection->query($sql_fech);
-
-//            if (!empty($result->fetch_assoc())) {
-//                echo "Вы авторизованны";
-//            }
-
         }else{
             echo "Пароли не совподают";
         }
@@ -29,31 +21,23 @@
 <?php
 
 
-    if (!empty($_POST['name_s']) and !empty($_POST['password_s'])){
-        if ($_POST['password'] == $_POST['password_check']) {
-            $name_s = $_POST['name_s'];
-            $password_s = $_POST['password_s'];
-            $sql = "SELECT * FROM users WHERE LastName=$name_s AND FirstName=$password_s ";
-            $connection->query($sql);
+    if (!empty($_POST['name_s']) and !empty($_POST['password_s'])) {
+        $name_s = $_POST['name_s'];
+        $password_s = $_POST['password_s'];
+        $sql_s = "SELECT * FROM users WHERE LastName='$name_s' AND FirstName='$password_s'";
+        $res_s = $connection->query($sql_s);
 
-
-    //            $sql_fech = "SELECT * FROM users WHERE LastName=$_POST[name] AND FirstName=$_POST[password]";
-    //            $result = $connection->query($sql_fech);
-
-    //            if (!empty($result->fetch_assoc())) {
-    //                echo "Вы авторизованны";
-    //            }
-
-        }else{
-            echo "Пароли не совподают";
+        if ($res_s->fetch_assoc()) {
+            echo "Вы зашли в свой аккаунт !";
         }
-    }else{
-        echo 'Вы не ввели данные';
+
     }
 
 ?>
 
+<?php
 //    $connection = mysqli_connect('mysql', 'root', 'root', 'PHP' );
+//    $connection->query("CREATE DATABASE PHP");
 
 //    $connection->query('CREATE TABLE users(
 //        PersonID int,
@@ -63,13 +47,13 @@
 //    $sql_q = "SELECT * FROM users";
 //    $sql = "INSERT INTO users (LastName, FirstName) VALUES ('Гаджимурад', 'Абакаров')";
 //
-//    $res = $connection->query($sql_q);
+//   $res = $connection->query($sql_q);
 //    var_dump($res->fetch_assoc());
-
-
-//    $result = $connection->query($sql);
-//    var_dump($result->fetch_assoc());
-
+//
+//
+//   $result = $connection->query($sql);
+//   var_dump($result->fetch_assoc());
+//
 
 
 
